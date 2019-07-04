07ffdfbe655405bbddd2f097919f16343b939f7d
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-07-04 18:37:11 -0400

    Add play_drums
    play_drums handles the logic behind animating the keys when they are pressed and playing the corresponding sound

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+function play_drums(key){
+    var sound = new Audio(`sounds/${key}.wav`);
+    const div_to_select = document.getElementById(key);
+    div_to_select.classList.add('button-pressed');
+    sound.play();
+    setTimeout(() => div_to_select.classList.remove('button-pressed'), 200);
+}
+
 function main(){
     window.addEventListener('keydown', (e) => {
         var valid_keys = ['d','k','o','p','s'];
